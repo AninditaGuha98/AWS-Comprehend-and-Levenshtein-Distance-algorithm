fcfc854d70a15bcbb3082a354643009aeb6b8ccf
Anindita Guha <an711755@dal.ca>
2020-07-04 03:14:11 -0300

    upload files to S3 done

---
+++ uploadFilesToS3.py
@@ -0,0 +1,14 @@
+author = "Anindita"
+
+import boto3
+import glob
+import time
+
+s3 = boto3.client('s3')
+counter = 1
+for file in glob.glob('Train/*.txt'):
+    s3.upload_file(file, "sourcedatab00845637", str(counter) + '.txt')
+    print("upload successful", counter)
+    input()
+    counter = counter + 1
+    time.sleep(0.1)
